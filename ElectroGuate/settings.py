b63201f4902110484b7aac3fbe4f446319423b42
ealvarado <quiqueal.17@hotmail.com>
2020-08-18 19:30:22 -0600

    Dev environment init

--- ElectroGuate/settings.py
+++ ElectroGuate/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'ElectroGuate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ElectroDB',
+        'USER': 'ElectroDev',
+        'PASSWORD': 'electro2020',
+        'HOST':'localhost',
+        'DATABASE_PORT': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators

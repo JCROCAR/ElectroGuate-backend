25731a4572bb962655c7c0bacf59606d19303450
jcrocar <juancarlos.rr97@gmail.com>
2020-09-07 12:16:46 -0600

    FIX: CORS
    Se agregó la configuración para las CORS en la API.

--- ElectroGuate/settings.py
+++ ElectroGuate/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
+    'corsheaders',
     'users'
 ]
 
@@ -60,7 +61,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'ElectroGuate.urls'
 

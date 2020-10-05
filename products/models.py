111611804611f21719998e88494971371085ff63
Oswaldo Alvarez <mynor4755@hotmail.com>
2020-10-04 18:18:45 -0600

    ADD: Brand - Se agregó    - Modelo    - Vistas    - Serializer de la marca

--- products/models.py
+++ products/models.py
@@ -26,6 +26,24 @@ class Category(BaseModel):
     str_description=models.CharField(max_length=45)
     str_image_link=models.ImageField(upload_to=upload_to, null=True)
 
+class Brand(BaseModel):
+    """
+    Upload image
+    """
+    def upload_to(instance, filename):
+        now = timezone.now()
+        base, extension = os.path.splitext(filename.lower())
+        milliseconds = now.microsecond // 1000
+        return f"users/{now:%Y%m%d%H%M%S}{milliseconds}{extension}"
+
+    """
+    Create a Brand
+    """
+    str_name=models.CharField(max_length=45)
+    str_description=models.CharField(max_length=45)
+    str_image_link=models.ImageField(upload_to=upload_to, null=True)
+    category = models.ForeignKey(Category, default=1, related_name='brands', on_delete=models.CASCADE)
+
 class Product(models.Model):
     """
     Upload image
@@ -45,13 +63,12 @@ class Product(models.Model):
     str_image_link = models.ImageField(_("Product_image"), upload_to=upload_to, null=True)
     int_amount = models.IntegerField(blank=False)
     int_price = models.FloatField(blank=False)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE, default=1, related_name='products')
+    brand = models.ForeignKey(Category, default=1, related_name='category', on_delete=models.CASCADE)
 
     class Meta:
+        unique_together = ['category', 'str_name']
         ordering = ['str_name']
 
-class DetailProduct(BaseModel):
-    """
-    Create a detail_product
-    """
-    category=models.ForeignKey(Category, on_delete=models.CASCADE)
-    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    def __unicode__(self):
+        return '%d: %s' % (self.id, self.str_name)
--- products/serializers.py
+++ products/serializers.py
@@ -1,43 +1,72 @@
 from rest_framework import serializers
-from .models import Product, Category
-
-"""
-Serializer for product
-"""
-
-
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = Product
-        fields = [
-            'id',
-            'str_name',
-            'str_description',
-            'str_product_code',
-            'str_image_link',
-            'int_amount',
-            'int_price',]
+from .models import Product, Category, Brand
 
 
 class CategorySerializerWrite(serializers.ModelSerializer):
     """
     Serializer to write data in Category
     """
+
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = [
+            "str_name",
+            "str_description",
+            "str_image_link",
+        ]
 
 
 class CategorySerializerRead(serializers.ModelSerializer):
     """
     Serializer to read data in Category
     """
+
     class Meta:
-        model=Category
+        model = Category
         fields = [
-            'id',
-            'str_name',
-            'str_description',
-            'str_image_link',
+            "id",
+            "str_name",
+            "str_description",
+            "str_image_link",
+        ]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    """
+    Serializer for product
+    """
+
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "str_name",
+            "str_description",
+            "str_product_code",
+            "str_image_link",
+            "int_amount",
+            "int_price",
+            "category",
+            "brand",
+        ]
+
+
+class BrandSerializerWrite(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = [
+            "str_name",
+            "str_description",
+            "str_image_link",
+            "category",
+        ]
+
+
+class BrandSerializerRead(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = [
+            "str_name",
+            "str_description",
+            "str_image_link",
         ]
